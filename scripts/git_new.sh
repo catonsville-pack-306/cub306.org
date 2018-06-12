a86d347891ba77272a1b44373d9b7a4dcf2f2440
Thomas Cherry <thomas.cherry@gmail.com>
2018-06-11 21:30:03 -0400

    updating the shell script to do a pull as needed

--- scripts/git_new.sh
+++ scripts/git_new.sh
@@ -1,8 +1,62 @@
 #!/bin/bash
 
-pushd /home/cubpack/cub306.org > /dev/null
-if [ -n "$(git fetch --dry-run)" ] ; then
-    git pull
-fi
-popd > /dev/null
+BRANCH=master
+SITE=cub306.org
+URL=https://github.com/catonsville-pack-306/cub306.org
 
+help()
+{
+    printf "Update repository on demand\n"
+    printf "cmd [dmh]\n"
+    t1="%5s %s\n"
+    printf "${t1}" "-d" "development"
+    printf "${t1}" "-h" "help"
+    printf "${t1}" "-m" "master"
+}
+
+work()
+{
+    if [ -a /home/cubpack/${SITE} ] ; then
+        pushd /home/cubpack/${SITE} > /dev/null
+        revision=$(git ls-remote $URL | grep ${BRANCH} | cut -f 1)
+        if [ -n "${revision}" ] ; then
+            # we got a revision number from curl
+            if [ -f ~/.last_revision_of_${BRANCH} ] ; then
+                old_revision=$(cat ~/.last_revision_of_${BRANCH})
+                if [ "${old_revision}" != "${revision}" ] ; then
+                    git pull
+                fi
+            else
+                #first time here
+                git pull
+            fi
+            echo ${revision} > ~/.last_revision_of_${BRANCH}
+        fi
+        popd > /dev/null
+    fi
+}
+
+while getopts "dhm" opt; do
+    # OPTARG - not used yet
+    case $opt in
+        d)
+            BRANCH="develop"
+            SITE="develop.cub306.org"
+            ;;
+        m)
+            BRANCH="master"
+            SITE="cub306.org"
+            ;;
+        h)
+            help
+            exit 0
+            ;;
+        *)
+            printf "invalid options\n"
+            help
+            exit 1
+            ;;
+    esac
+done
+
+work
