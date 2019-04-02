fad9ddeb16e220321b48095972dbc0c46646f772
Thomas Cherry <thomas.cherry@gmail.com>
2019-04-02 19:53:39 -0400

    adding table of contents to the global script

--- public/scripts/default.js
+++ public/scripts/default.js
@@ -36,3 +36,37 @@ $( document ).ready(function()
     });
     
 });
+
+/** create a table of contents */
+$( document ).ready(function()
+{
+    toc = "Table of Contents:<ol>"
+    $("h2").each(function(index)
+    {
+        let title = $(this).text();
+        if ($.trim(title)!="")
+        {
+            let hash = title.replace(/[^\w]/ig, "-")
+            $("<a name='" + hash + "'></a>").insertBefore(this);
+            toc += "<li><a href='#" + hash +"'>" + title + "</a></li>"
+        }
+    });
+    toc += "</ol>"
+    $("#toc").append(toc)
+    $("a[href='toc']").replaceWith(toc)
+});
+
+/*$( document ).ready(function()
+{
+    toc = "Table of Contents:<ol>"
+    $("h2").each(function(index)
+    {
+        let title = $(this).text();
+        let hash = title.replace(/[^\w]/ig, "-")
+        $("<a name='" + hash + "'></a>").insertBefore(this);
+        toc += "<li><a href='#" + hash +"'>" + title + "</a></li>"
+    });
+    toc += "</ol>"
+    
+    $("a[href='toc']").replaceWith(toc)
+});*/
