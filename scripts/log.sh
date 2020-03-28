d771a389013408ce77765cd380becde880faa4a2
Thomas Cherry <thomas.cherry@gmail.com>
2020-03-28 18:49:55 -0400

    adding more logs

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-log=~/logs/cub306.org/http
+LOG_MAIN=~/logs/cub306.org/http/access_log
+LOG_DEV=~/logs/develop.cub306.org/http/access_log
+log="${LOG_MAIN}"
 
 help()
 {
-    printf "Update repository on demand\n"
+    printf "Dump some simple log analyzes\n"
     printf "cmd [dmh]\n"
     t1="%5s %s\n"
     printf "${t1}" "-d" "development"
@@ -14,35 +16,57 @@ help()
 
 visitor_counts ()
 {
-    log_path=$1
+    log_path="${1}"
     grep ' 200 ' "${log_path}" \
+        | grep -v '\.css' \
+        | grep -v '\.js' \
         | awk '{print $1}' \
         | sort \
-        | uniq -c
+        | uniq -c \
+        | sort --numeric-sort --reverse \
+        | head -n 20
 }
 
 page_counts ()
 {
-    log_path=$1
-    #grep ' 200 ' "${log_path}"
+    log_path="${1}"
     grep ' 200 ' "${log_path}" \
-        | awk 'BEGIN { FPAT="([^ ]+)|(\"[^\"]+\")|(\\[[^\\]]+\\])" } { print $5 }' \
+        | grep -v '\.css' \
+        | grep -v '\.js' \
+        | gawk 'BEGIN { FPAT="([^ ]+)|(\"[^\"]+\")|(\\[[^\\]]+\\])" } { print $5 }' \
         | sort \
-        | uniq -c
+        | uniq -c \
+        | sort --numeric-sort --reverse \
+        | head -n 20
 }
 
+email_counts ()
+{
+    log_path="${1}"
+    grep -o '/images/PackLogo_.*\?\w\sHTTP' "${log_path}" \
+        | sed 's/HTTP//' \
+        | sort \
+        | uniq -c \
+        | sort --numeric-sort --reverse \
+        | head -n 20
+
+#grep 'GET /images/PackLogo_' ~/logs/cub306.org/https/access.log | sort | grep -o '/images/PackLogo_.*\?\w\sHTTP' | sed 's/HTTP//' |  uniq -c | sort --numeric-sort --reverse
+
+
+}
 
 work()
 {
     echo done
 }
 
-while getopts "chl:ps" opt; do
+while getopts "cehl:ps" opt; do
     # OPTARG - not used yet
     case $opt in
-        s) echo '----' ;;
-        p) page_counts "${log}" ;;
         c) visitor_counts "${log}" ;;
+        e) email_counts "${log}" ;;
+        p) page_counts "${log}" ;;
+        s) echo '----' ;;
         h)
             help
             exit 0
